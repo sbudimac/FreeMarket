61c7cc29bceeff0dbe0671c75673d0bd5855d912
sbudimac <sbudimac@gmail.com>
2025-12-06 18:16:37 +0100

    Initial Spring Security setup.

--- platform/src/main/java/com/freemarket/platform/config/SecurityConfig.java
+++ platform/src/main/java/com/freemarket/platform/config/SecurityConfig.java
@@ -4,9 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -17,13 +23,20 @@ public class SecurityConfig {
     }
 
     @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        UserDetails user = User.withUsername("admin")
+                .password(encoder.encode("admin"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Disable CSRF for POST requests
-                .authorizeHttpRequests(authz -> authz
-                        .anyRequest().permitAll() // Allow ALL requests without authentication
-                );
-
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .formLogin(withDefaults());
         return http.build();
     }
 }
--- platform/src/main/java/com/freemarket/platform/controller/AuthController.java
+++ platform/src/main/java/com/freemarket/platform/controller/AuthController.java
@@ -1,4 +1,13 @@
 package com.freemarket.platform.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
 }
