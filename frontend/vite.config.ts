44b6b0e6a88ae82bce7b34f0d41a786b7d18ab45
sbudimac <sbudimac@gmail.com>
2026-01-02 18:24:03 +0100

    Add Vite proxy for backend endpoints.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": "http://localhost:8080",
+      "/login": "http://localhost:8080",
+      "/logout": "http://localhost:8080",
+      "/register": "http://localhost:8080",
+      "/hello": "http://localhost:8080",
+    }
+  }
 })
